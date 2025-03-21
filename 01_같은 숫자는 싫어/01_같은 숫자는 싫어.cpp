0805283a110a8d134a810b94b048c53ec83f089a
성민 류 <baekhun0330@naver.com>
2025-03-21 16:26:41 +0900

    스택/큐 알고리즘

---
+++ 01_같은 숫자는 싫어/01_같은 숫자는 싫어.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+
+    //for (vector<int>::reverse_iterator riter = answer.rbegin(); riter != answer.rend(); ++riter)
+    //{
+    //    vector<int>::iterator i = riter.base();
+    //    *i = 4;
+    //    i;
+
+    //}
+
+    //for (size_t i = arr.size() - 1;)
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        int Value = arr[i];
+        if (0 == i)
+        {
+            answer.push_back(Value);
+            continue;
+        }
+        int Back = answer.back();
+        if (answer.back() == Value)
+        {
+            continue;
+        }
+        answer.push_back(Value);
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    vector<int> Result =  solution({ 4,4,3,3,3 });
+    for (int i = 0; i < Result.size(); i++)
+    {
+        std::cout << Result[i] << std::endl;
+    }
+}
+
---
+++ 02_기능개발/02_기능개발.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+//vector<int> solution(vector<int> progresses, vector<int> speeds) 
+//{
+//    vector<int> answer;
+//    std::queue<int> progressesQueue;
+//    std::queue<int> speedsQueue;
+//
+//    for (size_t i = 0; i < progresses.size(); i++)
+//    {
+//        progressesQueue.push(progresses[i]);
+//        speedsQueue.push(speeds[i]);
+//    }
+//
+//    int Day = 0;
+//    int Count = 0;
+//    while (true)
+//    {
+//        if (true == progressesQueue.empty())
+//        {
+//            answer.push_back(Count);
+//            return answer;
+//        }
+//        int& CurValue = progressesQueue.front();
+//        int Speed = speedsQueue.front();
+//
+//        if (100 > CurValue)
+//        {
+//            for (int i = 0; i < Day; i++)
+//            {
+//                CurValue += Speed;
+//            }
+//        }
+//
+//        if (100 <= CurValue)
+//        {
+//            progressesQueue.pop();
+//            speedsQueue.pop();
+//            ++Count;
+//            continue;
+//        }
+//
+//        if (0 != Count)
+//        {
+//            answer.push_back(Count);
+//            Count = 0;
+//        }
+//
+//        while (100 > CurValue)
+//        {
+//            CurValue += Speed;
+//            ++Day;
+//        }
+//    }
+//}
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+
+    int day;
+    int max_day = 0;
+    for (int i = 0; i < progresses.size(); ++i)
+    {
+        day = (99 - progresses[i]) / speeds[i] + 1;
+
+         if (answer.empty() || max_day < day)
+            answer.push_back(1);
+        else
+            ++answer.back();
+
+        if (max_day < day)
+            max_day = day;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::vector<int> Progresses = { 95, 90, 99, 99, 80, 99 };
+    std::vector<int> Speeds = { 1, 1, 1, 1, 1 ,1 };
+    std::vector<int> Result = solution(Progresses, Speeds);
+    std::cout << "Hello World!\n";
+}
