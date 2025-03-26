520491f4c24b1d898053beeae2ed1587a1f9b228
성민 류 <baekhun0330@naver.com>
2025-03-26 18:27:31 +0900

    레벨 2 스택_올바른 괄호

---
+++ 02_스택_올바른 괄호/02_스택_올바른 괄호.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+//bool solution(string s)
+//{
+// /*   std::stack<char> Stack;
+//    
+//    for (size_t i = 0; i < s.size(); i++)
+//    {
+//        char Char = s[i];
+//        if (Char == '(' || 
+//            Char == '{' || 
+//            Char == '['   )
+//        {
+//            Stack.push(Char);
+//            continue;
+//        }
+//
+//        if (true == Stack.empty())
+//        {
+//            return false;
+//        }
+//
+//        if (Char == ')' && Stack.top() == '(' ||
+//            Char == '}' && Stack.top() == '{' ||
+//            Char == ']' && Stack.top() == '[')
+//        {
+//            Stack.pop();
+//            continue;
+//        }
+//
+//        return false;
+//    }
+//
+//    if (true == Stack.empty())
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//    */
+//}
+
+bool solution(string s)
+{
+
+}
+
+int main()
+{
+    std::string String = "(()(";
+    bool Result = solution(String);
+    int a = 0;
+}
