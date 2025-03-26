87683e64b02a35f122572f307b85f87dd0c9bfb8
성민 류 <baekhun0330@naver.com>
2025-03-26 18:45:57 +0900

    저장

---
+++ 02_스택_다리를 지나는 트럭/02_스택_다리를 지나는 트럭.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) 
+{
+    int answer = 0;
+
+    return answer;
+}
+
+int main()
+{
+    std::vector<int> Vector{ 7, 4, 5, 6 };
+    int Result = solution(2, 10, Vector);
+}
