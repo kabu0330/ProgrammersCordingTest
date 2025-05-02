77739e391a59166146890f93b44b84f40d6dabde
성민 류 <baekhun0330@naver.com>
2025-05-02 18:43:57 +0900

    완주하지 못한 선수

---
+++ 01_완주하지 못한 선수/01_완주하지 못한 선수.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) 
+{
+	std::unordered_map<string, int> Count;
+	for (const string& Name : completion)
+	{
+		++Count[Name];
+	}
+
+	for (const string& Name : participant)
+	{
+		if (0 == Count[Name])
+		{
+			return Name;
+		}
+		--Count[Name];
+	}
+}
+
+int main()
+{
+    std::cout << solution({ "leo", "kiki", "eden", "leo"}, {"eden", "kiki", "leo"});
+}
+
