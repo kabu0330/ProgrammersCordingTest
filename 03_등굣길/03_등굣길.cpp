292222c4e8a4adc7a91f050130c8a3240b895b16
성민 류 <baekhun0330@naver.com>
2025-05-06 21:38:40 +0900

    등굣길

--- 03_등굣길/03_등굣길.cpp
+++ 03_등굣길/03_등굣길.cpp
@@ -1,20 +1,48 @@
-﻿// 03_등굣길.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <string>
+#include <vector>
 
-#include <iostream>
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) 
+{
+    std::vector<std::vector<int>> DP(n + 1, std::vector<int>(m + 1, 0));
+
+    // 물웅덩이
+    for (const std::vector<int>& Puddle : puddles)
+    {
+        int x = Puddle[0];
+        int y = Puddle[1];
+        DP[y][x] = -1;
+    }
+
+    DP[1][1] = 1;
+    for (int y = 1; y <= n; y++)
+    {
+        for (int x = 1; x <= m; x++)
+        {
+            if (DP[y][x] == -1)
+            {
+                DP[y][x] = 0;
+                continue;
+            }
+
+            if (DP[y - 1][x] > 0)
+            {
+                DP[y][x] += DP[y - 1][x];
+            }
+            if (DP[y][x -1] > 0)
+            {
+                DP[y][x] += DP[y][x - 1];
+            }
+            DP[y][x] %= 1000000007;
+        }
+    }
+    
+    return  DP[n][m];
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << solution(4, 3, { { 2, 2 } });
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
