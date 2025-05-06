55d26206e8dfd1f2b1216acf44afcd97b23dc028
성민 류 <baekhun0330@naver.com>
2025-05-06 17:23:15 +0900

    이중 우선순위 큐(multiset으로 해결)

--- 03_이중 우선순위 큐/03_이중 우선순위 큐.cpp
+++ 03_이중 우선순위 큐/03_이중 우선순위 큐.cpp
@@ -1,16 +1,51 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 vector<int> solution(vector<string> operations) 
 {
-    vector<int> answer;
+    vector<int> answer(2, 0);
+    std::multiset<int> Tree;
+    
+    for (const string& String : operations)
+    {
+        if ('I' == String[0]) // 삽입
+        {
+            int Num = std::stoi(String.substr(2));
+            Tree.insert(Num);
+        }
+        else if ("D 1" == String) // 최댓값 삭제
+        {
+            if (true == Tree.empty())
+            {
+                continue;
+            }
+            std::multiset<int>::iterator MaxIter = std::prev(Tree.end());
+            Tree.erase(MaxIter);
+        }
+        else if ("D -1" == String) // 최솟값 삭제
+        {
+            if (true == Tree.empty())
+            {
+                continue;
+            }
+            Tree.erase(Tree.begin());
+        }
+    }
+
+    if (false == Tree.empty())
+    {
+        answer[0] = *Tree.begin();
+        answer[1] = *std::prev(Tree.end());
+    }
+
     return answer;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    solution({ "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" });
 }
