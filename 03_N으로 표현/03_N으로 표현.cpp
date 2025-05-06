b6b9370c912f4032287c709bafd88e1d177db254
성민 류 <baekhun0330@naver.com>
2025-05-06 20:58:41 +0900

    N으로 표현

---
+++ 03_N으로 표현/03_N으로 표현.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(int N, int number) 
+{
+	if (N == number)
+	{
+		return 1;
+	}
+
+	std::vector<std::set<int>> DP(9);
+	int NN = 0;
+	for (int i = 1; i <= 8; i++)
+	{
+		NN = NN * 10 + N;
+		DP[i].insert(NN);
+
+		for (int j = 1; j < i; j++)
+		{
+			for (int A : DP[j])
+			{
+				for (int B : DP[i - j])
+				{
+					DP[i].insert(A + B);
+					DP[i].insert(A - B);
+					DP[i].insert(A * B);
+					if (0 != B)
+					{
+						DP[i].insert(A / B);
+					}
+				}
+			}
+		}
+
+		if (true == DP[i].count(number))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+    std::cout << solution(5, 12);
+}
