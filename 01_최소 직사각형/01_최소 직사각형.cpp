af6ad630e432156ce13a71fba4e6958d3a671782
성민 류 <baekhun0330@naver.com>
2025-05-06 17:52:41 +0900

    최소 직사각형

---
+++ 01_최소 직사각형/01_최소 직사각형.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) 
+{
+    for (size_t i = 0; i < sizes.size(); i++)
+    {
+        if (sizes[i][0] < sizes[i][1])
+        {
+            std::swap(sizes[i][0], sizes[i][1]);
+        }
+    }
+
+    int LeftMax = 0;
+    for (size_t i = 0; i < sizes.size(); i++)
+    {
+        if (LeftMax < sizes[i][0])
+        {
+            LeftMax = sizes[i][0];
+        }
+    }
+
+    int RightMax = 0;
+    for (size_t i = 0; i < sizes.size(); i++)
+    {
+        if (RightMax < sizes[i][1])
+        {
+            RightMax = sizes[i][1];
+        }
+    }
+
+    return LeftMax * RightMax;
+}
+
+int main()
+{
+    std::cout << solution({{ 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 }});
+}
+
