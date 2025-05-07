6b11d09a2382145cb531bda29e3953138eb5d4d3
성민 류 <baekhun0330@naver.com>
2025-05-08 01:23:22 +0900

    타겟 넘버 다른 풀이

--- 02_타겟 넘버/02_타겟 넘버.cpp
+++ 02_타겟 넘버/02_타겟 넘버.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
-
+#include <queue>
 using namespace std;
 
 int DFS(const std::vector<int>& _Numbers, int _Index, int _Sum, int _Target)
@@ -20,7 +20,36 @@ int DFS(const std::vector<int>& _Numbers, int _Index, int _Sum, int _Target)
 
 int solution(vector<int> numbers, int target) 
 {
-    return DFS(numbers, 0, 0, target);
+    //return DFS(numbers, 0, 0, target);
+	struct State
+	{
+		int Index = 0;
+		int Sum = 0;
+	};
+	
+	std::queue<State> Queue;
+	Queue.push({ 0, 0 });
+
+	int Count = 0;
+
+	while (false == Queue.empty())
+	{
+		State CurState = Queue.front();
+		Queue.pop();
+
+		if (CurState.Index == numbers.size())
+		{
+			if (CurState.Sum == target)
+			{
+				++Count;
+				continue;
+			}
+		}
+
+		Queue.push({ CurState.Index + 1, CurState.Sum + numbers[CurState.Index] });
+		Queue.push({ CurState.Index + 1, CurState.Sum - numbers[CurState.Index] });
+	}
+	return Count;
 }
 
 int main()
