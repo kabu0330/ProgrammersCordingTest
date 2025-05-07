5f8e4d5a85c556bbadfbbd256fbb0667471c552b
성민 류 <baekhun0330@naver.com>
2025-05-07 18:41:09 +0900

    타겟 넘버

---
+++ 02_타겟 넘버/02_타겟 넘버.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int DFS(const std::vector<int>& _Numbers, int _Index, int _Sum, int _Target)
+{
+	if (_Index == _Numbers.size())
+	{
+		return _Sum == _Target ? 1 : 0;
+	}
+	
+	int Count = 0;
+	Count += DFS(_Numbers, _Index + 1, _Sum + _Numbers[_Index], _Target);
+	Count += DFS(_Numbers, _Index + 1, _Sum - _Numbers[_Index], _Target);
+
+	return Count;
+}
+
+int solution(vector<int> numbers, int target) 
+{
+    return DFS(numbers, 0, 0, target);
+}
+
+int main()
+{
+    std::cout << solution({ 1, 1, 1, 1 }, 3);
+}
---
+++ 04_사칙연산/04_사칙연산.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int solution(vector<string> arr)
+{
+    std::vector<std::set<int>> DP;
+    return -1;
+}
+
+int main()
+{
+    std::cout << solution({ "1", "-", "3", "+", "5", "-", "8" });
+}
+
+
