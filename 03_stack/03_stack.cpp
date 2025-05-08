445fa0492df4bb03c4bf50fc23bd466b6202ef7d
성민 류 <baekhun0330@naver.com>
2025-05-08 13:35:30 +0900

    충돌 수정

---
+++ 03_stack/03_stack.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+void Reverse(vector<T>& Vec)
+{
+    stack<T> Stack;
+    for (T Value : Vec)
+    {
+        Stack.push(Value);
+    }
+
+    for (size_t i = 0; i < Vec.size(); i++)
+    {
+        Vec[i] = Stack.top();
+        Stack.pop();
+    }
+}
+
+int main()
+{
+    string Str1 = "Hello";
+    string Str2 = "ALGORITHM";
+
+    vector<int> Vec1{ 10, 20, 30, 40, 50 };
+
+    Reverse<int>(Vec1);
+
+    for (size_t i = 0; i < Vec1.size(); i++)
+    {
+        cout << Vec1[i] << endl;
+    }
+
+}
