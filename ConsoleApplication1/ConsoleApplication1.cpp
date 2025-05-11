cab67554b8813b3c4e9af43f16bdb763ba3be28f
성민 류 <baekhun0330@naver.com>
2025-05-11 20:54:59 +0900

    백준

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "|\\_/|" << std::endl;
+	std::cout << "|q p|   /}" << std::endl;
+	std::cout << "( 0 )\"\"\"\\" << std::endl;
+	std::cout << "|\"^\"`   |" << std::endl;
+	std::cout << "||_/=\\__|" << std::endl;
+
+
+
+}
---
+++ EquelStacks/EquelStacks.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3)
+{
+  
+    
+}
+
+int main()
+{
+    std::cout << equalStacks({ 3, 2, 1, 1, 1 }, { 4, 3, 2 }, { 1, 1, 4,1 });
+}
