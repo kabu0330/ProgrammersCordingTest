b11c7648b55bfdc19a5e6410ebc54db881cdc9d3
성민 류 <baekhun0330@naver.com>
2025-05-12 17:13:39 +0900

    2차원 배열 최댓값

---
+++ 01_최댓값/01_최댓값.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+
+int main()
+{
+    std::vector<std::vector<int>> Matrix(9, std::vector<int>(9, 0));
+
+	int Max = std::numeric_limits<int>::min();
+
+	std::pair<int, int> Index;
+	for (size_t y = 0; y < Matrix.size(); y++)
+	{
+		std::vector<int> Values(Matrix[0].size());
+		for (size_t x = 0; x < Matrix[0].size(); x++)
+		{
+			std::cin >> Values[x];
+			if (Values[x] > Max)
+			{
+				Max = Values[x];
+				Index.first = y + 1;
+				Index.second = x + 1;
+			}
+		}
+		Matrix[y] = Values;
+	}
+	std::cout << Max << std::endl;
+	std::cout << Index.first << " " << Index.second << std::endl;
+}
