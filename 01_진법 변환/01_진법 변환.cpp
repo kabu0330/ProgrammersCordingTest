ea3a47540c7388a7b8da45d0e5157a6cd412c1d2
성민 류 <baekhun0330@naver.com>
2025-05-12 17:51:33 +0900

    진법 변환

---
+++ 01_진법 변환/01_진법 변환.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+int main()
+{
+    std::string N = "";
+    int B = 0;
+    std::cin >> N >> B;
+        
+    long long Result = 0;
+    for (size_t i = 0; i < N.length(); i++)
+    {
+        char Char = N[i];
+        int Value = 0;
+        if ('0' <= Char && '9' >= Char)
+        {
+            Value = Char - '0';
+        }
+        else if ('A' <= Char && 'Z' >= Char)
+        {
+            Value = Char - 'A' + 10;
+        }
+
+        int Digit = N.length() - i - 1;
+        long long Power = 1;
+        for (int j = 0; j < Digit; j++)
+        {
+            Power *= B;
+        }
+        Result += static_cast<long long>(Value) * Power;
+    }
+
+    std::cout << Result << std::endl;
+}
