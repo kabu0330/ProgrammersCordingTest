174a2e558900e92387fb0317d031da53ef7c71d7
성민 류 <baekhun0330@naver.com>
2025-05-13 12:24:10 +0900

    배수와 약수

---
+++ 02_배수와 약수/02_배수와 약수.cpp
@@ -0,0 +1,61 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::vector<int> Inputs;
+	int Value = 0;
+	while (std::cin >> Value)
+	{
+		if (0 == Value)
+		{
+			break;
+		}
+		Inputs.push_back(Value);
+	}
+
+	int First = 0;
+	int Second = 0;
+	std::vector<std::string> Answer((Inputs.size() / 2) - 1, "");
+	int Index = 0;
+	for (size_t i = 0; i < Inputs.size(); i++)
+	{
+		if (1 == i % 2)
+		{
+			First = Inputs[i];
+			continue;
+		}
+		else
+		{
+			if (0 == First && 0 == Inputs[i])
+			{
+				break;
+			}
+
+			Second = Inputs[i];
+		}
+		
+		if (0 == Second % First)
+		{
+			Answer[Index] = "factor";
+		}
+		else if (0 == First % Second)
+		{
+			Answer[Index] = "multiple";
+		}
+		else
+		{
+			Answer[Index] = "neither";
+		}
+		
+		++Index;
+	}
+
+	for (size_t i = 0; i < Answer.size(); i++)
+	{
+		std::cout << Answer[i] << std::endl;
+	}
+}
+
