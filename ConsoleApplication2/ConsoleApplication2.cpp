de0fa84e9f4eeda80aa69be444f898e5a2900756
성민 류 <baekhun0330@naver.com>
2025-05-27 23:37:57 +0900

    저장

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
 int main()
 {
@@ -8,4 +9,9 @@ int main()
 
 	str.replace(7, 4, "Col");
 	std::cout << str << std::endl;
+
+	std::vector<int> vec = { 1, 4, 3, 4, 5, 4, 5 };
+	int result = std::count(vec.begin(), vec.end(), 5);
+
+	std::cout << result << std::endl;
 }
