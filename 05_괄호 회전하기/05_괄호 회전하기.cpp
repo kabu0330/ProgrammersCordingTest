6fe2079da1c4657c432129478d67615ec1d4539b
성민 류 <baekhun0330@naver.com>
2025-06-08 15:07:58 +0900

    괄호 회전하기

---
+++ 05_괄호 회전하기/05_괄호 회전하기.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+
+std::unordered_map<char, char> bracketPair = 
+{ // 닫힘, 열림   Pair
+    {')', '('}, 
+    {'}', '{'}, 
+    {'[', ']'} 
+};
+
+bool isValid(std::string& str, int start)
+{
+    std::stack<char> stack;
+    unsigned int size = str.size();
+    
+    for (int i = 0; i < size; i++)
+    {
+        int index = (start + i) % size;
+        char ch = str[index];
+
+        int a = bracketPair.count(ch);
+
+        if (1 == bracketPair.count(ch))
+        {
+            if (stack.empty() || stack.top() != bracketPair[ch])
+            {
+                return false;
+            }
+            
+            stack.pop();
+        }
+        else
+        {
+            stack.push(ch);
+        }
+    }
+
+    return stack.empty();
+}
+
+int solution(std::string str)
+{
+    int answer = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        answer += isValid(str, i);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::string str = "[](){}";
+    int result = solution(str);
+}
