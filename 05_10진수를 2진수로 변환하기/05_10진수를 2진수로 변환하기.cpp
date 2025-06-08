fda926a157f5fc3d98daef2d8f8385bc4ccd1cdd
성민 류 <baekhun0330@naver.com>
2025-06-08 14:40:04 +0900

    10진수를 2진수로 변환하기

---
+++ 05_10진수를 2진수로 변환하기/05_10진수를 2진수로 변환하기.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <stack>
+#include <string>
+
+std::string solution(int value)
+{
+	if (0 == value)
+	{
+		return "0";
+	}
+	std::stack<int> stack;
+	while (0 != value)
+	{
+		if (0 == (value % 2))
+		{
+			stack.push(0);
+		}
+		else
+		{
+			stack.push(1);
+		}
+		value /= 2;
+	}
+	std::string result;
+	while (false == stack.empty())
+	{
+		result += (char)(stack.top() + '0');
+		stack.pop();
+	}
+	return result;
+}
+
+int main()
+{
+	int decimal = 27;
+	std::string result = solution(decimal);
+}
