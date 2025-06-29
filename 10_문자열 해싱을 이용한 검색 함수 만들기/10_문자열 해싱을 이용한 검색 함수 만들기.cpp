57c024399ff8cb2e976e94edf6b2c8d41de9e7bb
성민 류 <baekhun0330@naver.com>
2025-06-29 15:12:10 +0900

    해시

---
+++ 10_문자열 해싱을 이용한 검색 함수 만들기/10_문자열 해싱을 이용한 검색 함수 만들기.cpp
@@ -0,0 +1,43 @@
+﻿
+#include "another.h"
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+vector<bool> solution(vector<string> string_list, vector<string> query_list)
+{
+	unordered_set<string> string;
+	for (int i = 0; i < string_list.size(); i++)
+	{
+		string.insert(string_list[i]);
+	}
+	
+	vector<bool> answer;
+	for (int i = 0; i < query_list.size(); i++)
+	{
+		if (string.end() != string.find(query_list[i]))
+		{
+			answer.push_back(true);
+		}
+		else
+		{
+			answer.push_back(false);
+		}
+	}
+
+	return answer;
+}
+
+
+
+int main()
+{
+	vector<string> string_list = { "apple", "banana", "cherry" };
+	vector<string> query_list = { "banana", "kiwi", "melon", "apple" };
+	vector<bool> result = solution(string_list, query_list);
+	vector<bool> result2 = solution2(string_list, query_list);
+}
+
---
+++ 10_완주하지 못한 선수/10_완주하지 못한 선수.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+
+using namespace std;
+
+
+string solution(vector<string> participant, vector<string> completion)
+{
+	unordered_map<string, int> names;
+	
+	for (const string& str : participant)
+	{
+		names[str]++;
+	}
+
+	for (string str : completion)
+	{
+		names[str]--;
+		if (names[str] == 0)
+		{
+			names.erase(str);
+		}
+	}
+
+	return names.begin()->first;
+}
+
+int main()
+{
+	vector<string> participant = { "mislav", "stanko", "mislav", "ana" };
+	vector<string> completion = { "stanko", "ana", "mislav" };
+	string result = solution(participant, completion);
+}
+
