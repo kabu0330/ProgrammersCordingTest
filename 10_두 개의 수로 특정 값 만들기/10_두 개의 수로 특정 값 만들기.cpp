a0e5b994787213284e1bb6c4587d88cb646905e9
성민 류 <baekhun0330@naver.com>
2025-06-29 13:31:06 +0900

    hash 기초

---
+++ 10_두 개의 수로 특정 값 만들기/10_두 개의 수로 특정 값 만들기.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+bool solution(vector<int> arr, int target)
+{
+	for (int i = 0; i < arr.size() - 1; i++)
+	{
+		for (int j = i + 1; j < arr.size(); j++)
+		{
+			if (target == arr[i] + arr[j])
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool solution2(vector<int> arr, int target)
+{
+	unordered_map<int, int> hash;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		hash.insert({ arr[i], 1 });
+	}
+	
+	for (int i = 0; i < arr.size(); i++)
+	{
+		int targetValue = target - arr[i];
+		if (hash.end() != hash.find(targetValue))
+		{
+			return true;
+		} 
+	}
+	return false;
+}
+
+int main()
+{
+	vector<int> arr = { 1, 2, 3, 4, 8 };
+	int target = 6;
+	bool result = solution(arr, target);
+	bool result2 = solution2(arr, target);
+
+}
+
