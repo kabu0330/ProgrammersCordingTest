ecaccc08e968694e982508aef893785b8957fc5b
성민 류 <baekhun0330@naver.com>
2025-06-30 13:58:08 +0900

    할인 행사

---
+++ 11_할인 행사/11_할인 행사.cpp
@@ -0,0 +1,85 @@
+﻿
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) 
+{
+    int answer = 0;
+
+    unordered_map<string, int> wantMap;
+    for (int i = 0; i < want.size(); i++)
+    {
+        wantMap[want[i]] = number[i];
+    }
+
+    for (int i = 0; i < discount.size() - 9; i++)
+    {
+        unordered_map<string, int> discount_10d;
+
+        for (int j = i; j < 10 + i; j++)
+        {
+            discount_10d[discount[j]]++;
+        }
+
+        if (wantMap == discount_10d)
+        {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
+
+int solution2(vector<string> want, vector<int> number, vector<string> discount)
+{
+    int answer = 0;
+
+    unordered_map<string, int> wantMap;
+    for (int i = 0; i < want.size(); i++)
+    {
+        wantMap[want[i]] = number[i];
+    }
+
+	unordered_map<string, int> discount_set;
+    for (int i = 0; i < 9; i++)
+    {
+        discount_set[discount[i]]++;
+    }
+
+    for (int i = 9; i < discount.size(); i++)
+    {
+        discount_set[discount[i]]++;
+        if (wantMap == discount_set)
+        {
+            answer++;
+        }
+        if (--discount_set[discount[i - 9]] == 0)
+        {
+            discount_set.erase(discount[i - 9]);
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<string> want0 = { "banana", "apple", "rice", "pork", "pot" };
+    vector<int> number0 = { 3, 2, 2, 2, 1 };
+    vector<string> discount0 = { "chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana" };
+
+    cout << solution(want0, number0, discount0) << endl;
+    cout << solution2(want0, number0, discount0) << endl;
+
+    vector<string> want1 = { "apple" };
+    vector<int> number1 = { 10 };
+    vector<string> discount1 = { "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana" };
+
+    cout << solution(want1, number1, discount1) << endl;
+    cout << solution2(want1, number1, discount1) << endl;
+}
