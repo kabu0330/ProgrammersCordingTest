b8d834bf3735b3bc2e2ed415f64fe9d0988d2d3d
성민 류 <baekhun0330@naver.com>
2025-07-01 12:24:56 +0900

    예상 대진표

---
+++ 12_예상 대진표/12_예상 대진표.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 1;
+    // a가 b와 만나는지 아는 법
+    // a가 홀수면 b가 a + 1인지?
+    // a가 짝수면 b가 a - 1인지?
+    // 둘 다 아니면 a,b 모두 2로 나누고 올림
+    while (true)
+    {
+        if (0 == a % 2 && b == a - 1)
+        {
+            return answer;
+        }
+        else if (1 == a % 2 && b == a + 1)
+        {
+            return answer;
+        }
+        
+        a = ceil((float)a / 2);
+        b = ceil((float)b / 2);
+        answer++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int n = 8;
+    int A = 4;
+    int B = 7;
+    int result = solution(n, A, B);
+}
+
---
+++ 12_이진 탐색 트리 구현/12_이진 탐색 트리 구현.cpp
@@ -0,0 +1,99 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node* left, * right;
+
+    Node(int key)
+        : val(key), left(nullptr), right(nullptr)
+    {
+
+    }
+};
+
+class BST
+{
+private:
+    Node* root;
+
+    Node* insertNode(Node* node, int key)
+    {
+        if (nullptr == node) 
+        {
+            return new Node(key);
+        }
+
+        if (key < node->val)
+        {
+            node->left = insertNode(node->left, key);
+        }
+        else
+        {
+            node->right = insertNode(node->right, key);
+        }
+        return node;
+    }
+
+    bool searchNode(Node* node, int key)
+    {
+        if (nullptr == node)
+        {
+            return false;
+        }
+
+        if (key == node->val)
+        {
+            return true;
+        }
+
+        return key < node->val ? searchNode(node->left, key) : searchNode(node->right, key);
+    }
+
+public:
+    BST()
+        :root(nullptr)
+    {
+
+    }
+
+    void insert(int key)
+    {
+        root = insertNode(root, key);
+    }
+
+    bool search(int key)
+    {
+        return searchNode(root, key);
+    }
+};
+
+vector<bool> solution(vector<int> lst, vector<int> search_lst)
+{
+    BST bst;
+    for (int key : lst)
+    {
+        bst.insert(key);
+    }
+
+    vector<bool> result;
+    for (int search_val : search_lst)
+    {
+        result.push_back(bst.search(search_val));
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> lst = { 5, 3, 8, 4, 2, 1, 7, 10 };
+    vector<int> search_lst = { 1,2,5,6 };
+    vector<bool> result = solution(lst, search_lst);
+}
+
