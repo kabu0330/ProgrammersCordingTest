7515d9cf0ed3cf19176afbfa63717cbb0f1b7a27
성민 류 <baekhun0330@naver.com>
2025-07-02 14:31:32 +0900

    폰켓몬

---
+++ 13_폰켓몬/13_폰켓몬.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer;
+
+    int choice = nums.size() / 2;
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+    answer = min(choice, (int)nums.size());
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> nums = { 3, 3, 3, 2, 2, 4 };
+    int result = solution(nums);
+}
+
