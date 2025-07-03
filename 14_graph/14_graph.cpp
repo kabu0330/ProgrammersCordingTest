4c1fbfad1dc500418177bc731b5e8879f19d4ac7
성민 류 <baekhun0330@naver.com>
2025-07-03 13:03:06 +0900

    그래프

---
+++ 14_graph/14_graph.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+struct nodeInfo
+{
+    int V; // 노드 번호
+    int W; // 가중치
+};
+
+vector<vector<nodeInfo>> adjList;
+
+void addEdge(int u, int v, int w)
+{
+    adjList[u].push_back({ v, w });
+}
+
+void printAdjList()
+{
+    for (int i = 0; i < adjList.size(); i++)
+    {
+        cout << "Node " << i << " : ";
+        for (const auto& node : adjList[i])
+        {
+            cout << " -> (V : " << node.V << ", W:" << node.W << ")";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int N = 5;
+    adjList.resize(N);
+
+    addEdge(1, 2, 3);
+    addEdge(2, 1, 6);
+
+    printAdjList();
+}
+
