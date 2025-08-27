328268128afdbabf6ea496c2d5a7354c1946469b
성민 류 <baekhun0330@naver.com>
2025-08-27 23:23:59 +0900

    2차원 배열

---
+++ 2차원배열/2차원배열.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    int arr[3][3] = {};
+
+    // 9 6 3  -3씩
+    // 8 5 2  -1씩
+
+    int left = 9;
+    int three = 3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        int right = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            arr[i][j] = left - (right * three);
+            ++right;
+        }
+        --left;
+    }
+}
+
