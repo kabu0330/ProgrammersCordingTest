687c3cf6799469d09d5960eeca4f78191883364f
성민 류 <baekhun0330@naver.com>
2025-08-28 12:42:45 +0900

    백트래킹

---
+++ 20_1부터N까지 숫자 중 합이 10이 되는 조합 구하기/20_1부터N까지 숫자 중 합이 10이 되는 조합 구하기.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> results;
+vector<int> selected_nums;
+
+void backtrack(int num, int sum, int start)
+{
+	if (sum == 10)
+	{
+		results.push_back(selected_nums);
+		return;
+	}
+	for (int i = start; i <= num; ++i)
+	{
+		if (sum + i <= 10)
+		{
+			selected_nums.push_back(i);
+			backtrack(num, sum + i, i + 1);
+			selected_nums.pop_back();
+		}
+	}
+}
+
+vector<vector<int>> solution(int n)
+{
+	backtrack(n, 0, 1);
+
+	return results;
+}
+
+int main()
+{
+	int n = 5;
+	solution(n);
+}
