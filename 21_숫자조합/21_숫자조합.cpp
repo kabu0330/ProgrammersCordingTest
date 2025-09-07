b3d3b9415a6a0a9350a58543e7856f0550b00319
성민 류 <baekhun0330@naver.com>
2025-09-07 17:58:24 +0900

    숫자조합

---
+++ 21_숫자조합/21_숫자조합.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> vec;
+
+void recursive(int start, int max, vector<int>& vec, vector<vector<int>>& result)
+{
+	int nums = 0;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		nums += vec[i];
+		if (nums > 10)
+		{
+			return;
+		}
+	}
+	if (nums == 10)
+	{
+		result.push_back(vec);
+		return;
+	}
+
+	for (int i = start; i <= max; i++)
+	{
+		vec.push_back(i);
+		recursive(i + 1, max, vec, result);
+		vec.pop_back();
+	}
+}
+
+vector<vector<int>> solution(int n)
+{
+	vector<vector<int>> result;
+
+	recursive(1, n, vec, result);
+
+	return result;
+}
+
+int main()
+{
+	int n = 7;
+	vector<vector<int>> result = solution(n);
+}
