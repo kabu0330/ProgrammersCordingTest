1ea557b4532ff2e3b1110b020ebc7ed089dcb0da
성민 류 <baekhun0330@naver.com>
2025-09-10 17:19:13 +0900

    피로도

---
+++ 22_피로도/22_피로도.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int max_dungeon = 0;
+
+void backtrack(int current_k, int count, vector<bool>& visited, const vector<vector<int>>& dungeons)
+{
+    max_dungeon = ::max(max_dungeon, count);
+
+    for (int i = 0; i < dungeons.size(); i++)
+    {
+        if (!visited[i] && current_k >= dungeons[i][0])
+        {
+            visited[i] = true;
+            backtrack(current_k - dungeons[i][1], count + 1, visited, dungeons);
+            visited[i] = false;
+            
+        }
+    }
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    vector<bool> visited(dungeons.size(), false);
+    backtrack(k, 0, visited, dungeons);
+    return max_dungeon;
+}
+
+int main()
+{
+    // 피로도
+    int k = 80; 
+
+    // 입장 컷 피로도, 소모 피로도
+    vector<vector<int>> dungeons = { {80, 20}, {50, 40}, {30, 10} };
+    int result = solution(k, dungeons);
+}
+
