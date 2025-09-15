b12695e2e023bb3b4e4616c8ba8d3a545490d8e3
성민 류 <baekhun0330@naver.com>
2025-09-15 21:47:49 +0900

    분해합

---
+++ 25_분해합/25_분해합.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i < n; i++)
+	{
+		int arr[8] = {};
+		int idx = 0;
+		int temp = i;
+		while (temp > 0)
+		{
+			arr[idx] = temp % 10;
+			temp /= 10;
+			++idx;
+		}
+
+		int arr_sum = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			arr_sum += arr[i];
+		}
+
+		int result = i + arr_sum;
+		if (result == n)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+
+	cout << 0;
+}
---
+++ 25_블랙잭/25_블랙잭.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* 1. 합이 21을 초과하지 않는 가장 큰 값을 구하는 게임
+*  2. 숫자 3개를 골라 M을 넘지 않으면서 가장 가까운 수
+*/
+
+int n, m;
+vector<int> cards;
+vector<int> is_used;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	cards.resize(n);
+	is_used.resize(n, 0);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> cards[i];
+	}
+
+	int best = 0;
+
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = i + 1; j < n - 1; j++)
+		{
+			for (int k = j + 1; k < n; k++)
+			{
+				int card_sum = cards[i] + cards[j] + cards[k];
+				if (card_sum <= m && card_sum > best)
+				{
+					best = card_sum;
+					if (best == m)
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+	cout << best;
+}
+
