8fc9ff4547b620e025cc6fb3dfe9f61c9dd9c455
성민 류 <baekhun0330@naver.com>
2025-09-20 07:11:27 +0900

    바이러스

---
+++ 27_바이러스/27_바이러스.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<vector<int>> graph;
+vector<int> visited;
+vector<int> result;
+
+void solve()
+{
+	queue<int> q;
+	q.push(1);
+	visited.resize(n + 1);
+	visited[1] = 1;
+
+	while (!q.empty())
+	{
+		int node = q.front();
+		q.pop();
+
+		for (auto& adj_node : graph[node])
+		{
+			if (visited[adj_node] == 0)
+			{
+				visited[adj_node] = 1;
+				q.push(adj_node);
+				result.push_back(adj_node);
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+
+	graph.resize(n + 1);
+	for (int i = 0; i < m; i++)
+	{
+		int k, w;
+		cin >> k >> w;
+		graph[k].push_back(w);
+		graph[w].push_back(k);
+	}
+	
+	solve();
+
+	cout << result.size();
+}
+
