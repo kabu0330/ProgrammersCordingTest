3b203a4589af20f9fd57c0d78895f8e5930a3345
성민 류 <baekhun0330@naver.com>
2025-09-20 13:14:54 +0900

    N_Queen

---
+++ 27_N-Queen/27_N-Queen.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> cols;
+int solution_count = 0;
+
+bool is_safe(int row, int col)
+{
+	for (int prev_row = 1; prev_row < row; ++prev_row)
+	{
+		if (cols[prev_row] == col)
+		{
+			return false;
+		}
+		if (abs(row - prev_row) == abs(col - cols[prev_row]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void solve(int row)
+{
+	if (row > n)
+	{
+		++solution_count;
+		return;
+	}
+	for (int col = 1; col <= n; col++)
+	{
+		if (is_safe(row, col))
+		{
+			cols[row] = col;
+			solve(row + 1);
+		}
+	}
+}
+
+int main()
+{
+	cin >> n;
+	cols.resize(n + 1);
+
+	solve(1);
+	cout << solution_count;
+}
+
