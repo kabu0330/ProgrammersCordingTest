ed89c8854c8568f7802fdfd07f6bfa4acae278ad
성민 류 <baekhun0330@naver.com>
2025-09-24 19:15:09 +0900

    영화감독 숌

---
+++ 29_영화감독 숌/29_영화감독 숌.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	
+	int count = 0;
+	int number = 665;
+
+	while (count < n)
+	{
+		++number;
+
+		string str = to_string(number);
+
+		if (str.find("666") != string::npos)
+		{
+			++count;
+		}
+	}
+
+	cout << number;
+}
+
