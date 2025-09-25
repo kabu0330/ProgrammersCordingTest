b1cab9e5eeab092709ccd225cc8881e1032a14a4
성민 류 <baekhun0330@naver.com>
2025-09-25 12:39:40 +0900

    설탕 배달

---
+++ 30_설탕 배달 풀이2/30_설탕 배달 풀이2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int count = 0;
+    while (n >= 0)
+    {
+        if (n % 5 == 0)
+        {
+            count += n / 5;
+            cout << count;
+            return 0;
+        }
+
+        n -= 3;
+        ++count;
+    }
+
+    cout << -1;
+}
---
+++ 30_설탕 배달/30_설탕 배달.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void solve(int n)
+{
+    int mod = n % 5;
+    if (mod == 0)
+    {
+        cout << n / 5;
+        return;
+    }
+
+    if (mod == 3)
+    {
+        //          5 kg 개수 + 3kg 1개
+        cout << ((n - 3) / 5) + 1;
+        return;
+    }
+    
+    for (int i = 1; i <= (n / 5); i++)
+    {
+        // 3 = 17 / 5 ... 2
+        int value = n / 5;
+        int five_sugar = (value - i) * 5;
+        if ((n - five_sugar) % 3 == 0)
+        {
+            int three_sugar = (n - five_sugar) / 3;
+            cout << (five_sugar / 5) + three_sugar;
+            return;
+        }
+    }
+
+    cout << -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    solve(n);
+   
+}
+
