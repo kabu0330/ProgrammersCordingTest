401a05f275d3a303a6bbc2c4c9e2605909b09f8e
성민 류 <baekhun0330@naver.com>
2025-09-26 11:53:04 +0900

    문자열 집합

---
+++ 31_문자열 집합/31_문자열 집합.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+	
+	unordered_set<string> str_set;
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		str_set.insert(str);
+	}
+
+	int match_count = 0;
+	for (int i = 0; i < m; i++)
+	{
+		string find_str;
+		cin >> find_str;
+		if (str_set.find(find_str) != str_set.end())
+		{
+			++match_count;
+		}
+	}
+
+	cout << match_count;
+}
+
