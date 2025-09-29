b6f8a35b240330090ce34d66418a731125bb17ad
성민 류 <baekhun0330@naver.com>
2025-09-29 13:53:23 +0900

    파도반 수열

---
+++ 32_파도반 수열/32_파도반 수열.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    
+    vector<int> inputs(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> inputs[i];
+    }
+
+    size_t dp[101] = {};
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 2;
+    dp[5] = 2;
+
+    for (int i = 6; i < 101; i++)
+    {
+        dp[i] = dp[i - 2] + dp[i - 3];
+       //dp[i] = dp[i - 1] + dp[i - 5]; 이것도 가능
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << dp[inputs[i]] << "\n";
+    }
+}
+
