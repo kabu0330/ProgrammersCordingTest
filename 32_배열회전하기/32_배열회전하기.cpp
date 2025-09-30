16c6d156956c4d84fe6eee7f756bb53ecb83f6fa
성민 류 <baekhun0330@naver.com>
2025-09-30 13:51:04 +0900

    배열 회전하기

---
+++ 32_배열회전하기/32_배열회전하기.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> rotate_90(const vector<vector<int>>& arr)
+{
+	int n = arr.size();
+	vector<vector<int>> rotated_arr(n, vector<int>(n, 0));
+
+	for (int y = 0; y < n; y++)
+	{
+		for (int x = 0; x < n; x++)
+		{
+			// rotated_arr[0][3] = arr[0][0]
+			// rotated_arr[0][2] = arr[0][1]
+			// rotated_arr[0][1] = arr[0][2]
+			// rotated_arr[0][0] = arr[0][3]
+			rotated_arr[x][n - y - 1] = arr[y][x];
+		}
+	}
+	return rotated_arr;
+}
+
+vector<vector<int>> solution(vector<vector<int>> arr, int n)
+{
+	vector<vector<int>> rotated_arr = arr;
+	for (int i = 0; i < n; i++)
+	{
+		rotated_arr = rotate_90(rotated_arr);
+	}
+	return rotated_arr;
+}
+
+int main()
+{
+	vector<vector<int>> arr = {
+		{ 1,  2,  3,  4},
+		{ 5,  6,  7,  8},
+		{ 9, 10, 11, 12},
+		{13, 14, 15, 16}
+	};
+
+	solution(arr, 2);
+
+}
+
---
+++ 32_튜플2/32_튜플2.cpp
@@ -0,0 +1,61 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+
+    // 숫자가 많이 등장한 순서대로 내림차순해야 함
+    // 해당 숫자가 등장한 빈도만큼 해당 인덱스에 카운팅
+    int counts[100001] = {};
+
+    // 문자열 파싱
+    string num = "";
+    for (const auto& ch : s)
+    {
+        // 숫자면 기록
+        if (isdigit(ch))
+        {
+            num += ch;
+        }
+        else // 숫자가 아니면 숫자 카운트
+        {
+            if (!num.empty()) 
+            {
+                int idx = stoi(num);
+                ++counts[idx];
+                num.clear();
+            }
+        }
+    }
+
+    vector<pair<int, int>> datas;
+    for (int i = 1; i < 100001; i++)
+    {
+        if (counts[i] > 0)
+        {
+            datas.push_back({counts[i], i});
+        }
+    }
+
+    // 숫자가 많이 등장한 순서대로 내림차순
+    sort(datas.rbegin(), datas.rend());
+
+    // 정답 입력
+    for (const auto& pair : datas)
+    {
+        answer.push_back(pair.second);
+    }
+
+    return answer;
+}
+int main()
+{
+    string str = "{{2}, {2,1}, {2,1,3}, {2,1,3,4}}";
+    solution(str);
+}
+
