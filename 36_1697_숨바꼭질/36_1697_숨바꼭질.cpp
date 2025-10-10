1e9fa252d6bef6159d32b35ea3966eb6df4965ca
성민 류 <baekhun0330@naver.com>
2025-10-10 22:38:22 +0900

    숨바꼭질

---
+++ 36_1697_숨바꼭질/36_1697_숨바꼭질.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int visited[1000001] = {};
+
+int n, k;
+int cnt = 0;
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	//    자신 동생
+	cin >> n >> k;
+
+	//    현재 위치, 시간
+	queue<int> q;
+	q.push(n);
+	visited[n] = 1;
+
+	while (!q.empty())
+	{
+		int node = q.front();
+		q.pop();
+
+		for (int next : {node + 1, node - 1, node * 2})
+		{
+			if (next < 0 || next >= 100001) continue;
+			if (visited[next]) continue;
+					
+			visited[next] = visited[node] + 1;
+			q.push(next);
+		}
+	}
+
+	cout << visited[k] - 1;
+}
+
