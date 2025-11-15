6c21e0829f8ac62fc0488045b1aad5df4fbb4e2f
성민 류 <baekhun0330@naver.com>
2025-11-15 15:11:36 +0900

    백트래킹

---
+++ 41_로또/main.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+vector<int> vec;
+vector<int> result;
+int target = 6;
+
+void solve(int start, int depth)
+{
+	if (depth == target)
+	{
+		for (int ele : result)
+		{
+			cout << ele << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = start; i < vec.size(); i++)
+	{
+		result.push_back(vec[i]);
+		solve(i + 1, depth + 1);
+		result.pop_back();
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	while (true)
+	{
+		int k;
+		cin >> k;
+		if (k == 0)
+		{
+			break;
+		}
+		vec.clear();
+		vec.resize(k);
+		for (int i = 0; i < k; i++)
+		{
+			cin >> vec[i];
+		}
+
+		solve(0, 0);
+		cout << '\n';
+	}
+}
+
---
+++ BackjoonCodingTestTemplate/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n;
+
+
+void solve()
+{
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+
+	solve();
+
+
+	cout << "정답" << endl;
+
+
+}
+
