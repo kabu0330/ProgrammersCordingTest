53c3a83f61c0cbce45218e8841ce05ecf0e0086d
성민 류 <baekhun0330@naver.com>
2025-11-15 21:30:13 +0900

    그리디

---
+++ 41_1026_보물/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n;
+
+vector<int> a;
+vector<int> b;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	
+	a.resize(n);
+	b.resize(n);
+	for (int j = 0; j < n; j++)
+	{
+		cin >> a[j];
+	}
+	for (int j = 0; j < n; j++)
+	{
+		cin >> b[j];
+	}
+	
+	::sort(a.begin(), a.end());
+	::sort(b.rbegin(), b.rend());
+
+	int result = 0;
+	for (int i = 0; i < n; i++)
+	{
+		result += a[i] * b[i];
+	}
+
+
+	cout << result;
+
+
+}
+
---
+++ 41_2217_로프/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	vector<int> vec(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i];
+	}
+
+	::sort(vec.rbegin(), vec.rend());
+	
+	int weight = vec[0];
+	for (int i = 0; i < vec.size(); i++)
+	{
+		weight = ::max(weight, vec[i] * (i + 1));
+	}
+
+	cout << weight;
+}
+
