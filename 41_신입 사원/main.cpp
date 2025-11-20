8da3d803f8f0687669b42f09e24472d9ff454db5
성민 류 <baekhun0330@naver.com>
2025-11-21 03:28:00 +0900

    그리디

--- 41_신입 사원/main.cpp
+++ 41_신입 사원/main.cpp
@@ -9,26 +9,41 @@
 using namespace std;
 
 // 전역 변수
-int n;
-
-
-void solve()
-{
-
-}
+int t;
 
 int main()
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> n;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		vector<pair<int, int>> vec;
+		
+		for (int i = 0; i < n; i++)
+		{
+			int a, b;
+			cin >> a >> b;
+			vec.push_back({ a, b });
+		}
 
-
-	solve();
-
-
-	cout << "정답" << endl;
-
+		sort(vec.begin(), vec.end());
+		
+		int bestB = vec[0].second;
+		int ret = 0;
+		ret++;
+		for (int i = 0; i < n; i++)
+		{
+			if (bestB > vec[i].second)
+			{
+				ret++;
+				bestB = vec[i].second;
+			}
+		}
+		cout << ret << endl;
+	}
 
 }
 
