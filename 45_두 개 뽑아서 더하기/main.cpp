51ba304d4aa202b8a90150549ceb3d5674ba6d7c
성민 류 <baekhun0330@naver.com>
2025-11-23 13:41:18 +0900

    배열

---
+++ 45_두 개 뽑아서 더하기/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define endl '\n'
+
+using namespace std;
+
+
+vector<int> solution(vector<int> numbers)
+{
+	set<int> s;
+	for (int i = 0; i < numbers.size() - 1; i++)
+	{
+		for (int j = i + 1; j < numbers.size(); j++)
+		{
+			s.insert(numbers[i] + numbers[j]);
+		}
+	}
+
+	vector<int> ret;
+	for (auto& ele : s)
+	{
+		ret.push_back(ele);
+		//cout << ele << " ";
+	}
+	return ret;
+}
+
+int main()
+{
+	vector<int> numbers = { 2, 1, 3, 4, 1 };
+	vector<int> result = solution(numbers);
+}
+
---
+++ 45_모의고사/main.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+vector<int> one = { 1, 2, 3, 4, 5 };
+vector<int> two = { 2, 1, 2, 3, 2, 4, 2, 5 };
+vector<int> three = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+vector<int> solution(vector<int> vec)
+{
+	vector<int> ret;
+	vector<int> matchCnt(3);
+	
+	for (int i = 0; i < vec.size(); i++)
+	{
+		if (one[i % one.size()] == vec[i])
+		{
+			matchCnt[0]++;
+		}
+		if (two[i % two.size()] == vec[i])
+		{
+			matchCnt[1]++;
+		}
+		if (three[i % three.size()] == vec[i])
+		{
+			matchCnt[2]++;
+		}
+	}
+
+	// 최고점을 추출해서 비교하면 쉽다.
+	int mn = *max_element(matchCnt.begin(), matchCnt.end());
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (matchCnt[i] == mn)
+		{
+			ret.push_back(i + 1);
+		}
+	}
+
+	for (auto& ele : ret)
+	{
+		cout << ele << " ";
+	}
+	return ret;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	vector<int> input = { 1, 2, 3, 4, 5 };
+
+
+	vector<int> ret = solution(input);
+}
+
