335817546a1aa2ea996ea937e727f2ffa68e9acb
성민 류 <baekhun0330@naver.com>
2025-11-25 00:02:20 +0900

    dp

---
+++ 46_LCS 길이 계산하기/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+int solution(string str1, string str2)
+{
+	vector<vector<int>> dp(str1.size() + 1, vector<int>(str2.size() + 1, 0));
+
+	for (int i = 1; i <= str1.size(); i++)
+	{
+		for (int j = 1; j <= str2.size(); j++)
+		{
+			// 현재 비교하는 문자가 같으면
+			if (str1[i - 1] == str2[j - 1])
+			{
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else
+			{
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+			cout << dp[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return dp[str1.size()][str2.size()];
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+	int ret = solution("ABCBDAB", "BDCAB");
+	cout << ret;
+}
+
---
+++ 46_LIS 계산하기/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+	vector<int> vec;
+
+	for (int num : nums)
+	{
+		// num 값 이상인 첫번째 위치
+		auto pos = lower_bound(vec.begin(), vec.end(), num);
+
+		// 존재하지 않으면 추가
+		if (pos == vec.end())
+		{
+			vec.push_back(num); 
+		}
+		else
+		{
+			*pos = num; // 더 작은 값으로 바꿔버린다.
+		}
+	}
+
+	return vec.size();
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	vector<int> input = { 1, 4, 2, 3, 1, 5, 7, 3 };
+
+
+	int ret = solution(input);
+	cout << ret;
+}
+
