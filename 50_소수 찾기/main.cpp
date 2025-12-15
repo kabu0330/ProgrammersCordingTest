a7418cb68660ba423d47f1fe416bdcc6a65fd2bf
kabu0330 <151612250+kabu0330@users.noreply.github.com>
2025-12-16 00:15:02 +0900

    구현

---
+++ 50_소수 찾기/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n;
+
+bool isPrime(int num)
+{
+	if (num < 2) return false;
+
+	for (int i = 2; i * i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+	int cnt = 0;
+	for (size_t i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+
+		if (isPrime(a))
+		{
+			++cnt;
+		}
+	}
+
+	cout << cnt << endl;
+}
+
---
+++ 50_직각삼각형/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n, m, k;
+
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	while (true)
+	{
+		vector<int> vec(3);
+		cin >> vec[0] >> vec[1] >> vec[2];
+		
+		if (vec[0] == 0 && vec[1] == 0 && vec[2] == 0) break;
+
+		::sort(vec.rbegin(), vec.rend());
+		
+		int a = vec[0];
+		int b = vec[1];
+		int c = vec[2];
+
+		if (a * a == b * b + c * c)
+		{
+			cout << "right" << endl;
+		}
+		else
+		{
+			cout << "wrong" << endl;
+		}
+	}
+
+}
+
