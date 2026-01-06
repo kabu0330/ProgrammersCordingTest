58913dff4e0e318bd9e6d7234f4b73599927f2df
kabu0330 <151612250+kabu0330@users.noreply.github.com>
2026-01-07 08:52:01 +0900

    백준

---
+++ 54_나는야 포켓몬 마스터/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n;
+int m;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); std::cout.tie(0);
+
+	cin >> n >> m;
+
+	vector<string> vec(n + 1);
+	unordered_map<string, int> hash;
+	hash.reserve(n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		string str;
+		cin >> str;
+		
+		vec[i] = str;
+		hash.emplace(str, i);
+	}
+	
+	while (m--)
+	{
+		string str;
+		cin >> str;
+
+		if (isdigit(str[0]))
+		{
+			int idx = stoi(str);
+			cout << vec[idx] << endl;
+		}
+		else
+		{
+			cout << hash[str] << endl;
+		}
+	}
+
+}
+
---
+++ 54_집합/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n;
+vector<bool> vec(21);
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	unsigned int bit_set = 0;
+
+	cin >> n;
+	while (n--)
+	{
+		string str;
+		cin >> str;
+
+		int num;
+
+		if (str == "add")
+		{
+			cin >> num;
+			bit_set |= (1 << num);
+		}
+		else if (str == "remove")
+		{
+			cin >> num;
+			bit_set &= ~(1 << num);
+		}
+		else if (str == "check")
+		{
+			cin >> num;
+			if (bit_set & (1 << num))
+				cout << 1 << endl;
+			else
+				cout << 0 << endl;
+		}
+		else if (str == "toggle")
+		{
+			cin >> num;
+			bit_set ^= (1 << num);
+		}
+		else if (str == "all")
+		{
+			bit_set = (1 << 21) - 1;
+		}
+		else if (str == "empty")
+		{
+			bit_set = 0;
+		}
+	}
+}
+
