a4e0cc40d21e5a6fa8a969df8422b53bca0c83c8
kabu0330 <151612250+kabu0330@users.noreply.github.com>
2026-01-08 19:41:11 +0900

    커밋

---
+++ 55_비밀번호 찾기/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n, m;
+
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	
+	unordered_map<string, string> hash;
+	hash.reserve(n);
+	
+	while (n--)
+	{
+		string site;
+		string pw;
+		cin >> site >> pw;
+		hash.insert({ site, pw });
+	}
+
+	while (m--)
+	{
+		string site;
+		cin >> site;
+		if (hash.find(site) != hash.end())
+		{
+			cout << hash[site] << endl;
+		}
+	}
+}
+
---
+++ 55_피보나치 함수/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+#define endl '\n'
+
+using namespace std;
+
+// 전역 변수
+int n;
+
+struct Count
+{
+	int zero = 0;
+	int one = 0;
+};
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+	vector<Count> dp(41);
+
+	dp[0] = { 1, 0 };
+	dp[1] = { 0, 1 };
+
+	for (int i = 2; i <= 40; i++)
+	{
+		dp[i].zero = dp[i - 1].zero + dp[i - 2].zero;
+		dp[i].one = dp[i - 1].one + dp[i - 2].one;
+	}
+
+	while (n--)
+	{
+		int n;
+		cin >> n;
+		cout << dp[n].zero << ' ' << dp[n].one << endl;
+	}
+
+}
+
